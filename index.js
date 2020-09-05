fae12d6bc71dd4012db9d01b476d0a6b68da6dec
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-09-05 20:52:37 +0300

    Fix old mistakes

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// function rectSize(){
-//     let item = document.getElementsByClassName('size') 
-//         for(let i in item){
-//             let size = item[0].clientWidth
-            
-//           //  item[i].style.height = size + 'px'
-//         }
-//         console.log(item[0].clientHeight ,item[0].clientWidth )
-// }
-
+ymaps.ready(init);
+function init(){  
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 14
+    });
+}
